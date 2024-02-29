9b69cc459ca3a8bbda4d3c2b1111a6178b221b82
MAlonsoMA <MAlonsoMA@users.noreply.github.com>
2024-02-29 16:26:39 +0100

    Aplicación servicios terminada. Configuración panel administración. Subir y ver imágenes. Mostrar servicios en el documento html. Ubicar las views y urls en la propia aplicación servicios. Termina de configurar la aplicación y modificar el CSS.

--- Proyectoweb/settings.py
+++ Proyectoweb/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ProyectowebApp',
+    'servicios',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-eu'
 
 TIME_ZONE = 'UTC'
 
@@ -117,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL='/media/'
+#MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_ROOT=(BASE_DIR / 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- ProyectowebApp/views.py
+++ ProyectowebApp/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render,HttpResponse
 
+from servicios.models import Servicio
+
 # Create your views here.
 
 def home(request):
     return render(request,"ProyectowebApp/home.html")
-
-def servicios(request):
-    return render(request,"ProyectowebApp/servicios.html")
 
 def tienda(request):
     return render(request,"ProyectowebApp/tienda.html")
