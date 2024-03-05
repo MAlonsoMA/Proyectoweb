9c282ff3c644391ff8356af9b5ce9add7828dc31
MAlonsoMA <MAlonsoMA@users.noreply.github.com>
2024-03-05 16:37:36 +0100

    Dejamos la aplicación tienda y creamos la aplicación que manejará el carro de la comnpra

--- ProyectowebApp/views.py
+++ ProyectowebApp/views.py
@@ -7,7 +7,5 @@ from django.shortcuts import render,HttpResponse
 def home(request):
     return render(request,"ProyectowebApp/home.html")
 
-def tienda(request):
-    return render(request,"ProyectowebApp/tienda.html")
 
 
---
+++ tienda/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import CategoriaProd,Producto
+
+# Register your models here.
+class CategoriaAdmin(admin.ModelAdmin):
+    readonly_fields=('created','updated')
+    list_display=("nombre","id")
+
+class ProductoAdmin(admin.ModelAdmin):
+    readonly_fields=('created','updated')
+    list_display=("nombre","categorias","imagen","precio","created","updated")
+    search_fields=("nombre","precio")
+    list_filter=("categorias","created")
+    date_hierarchy=("created")
+
+admin.site.register(CategoriaProd,CategoriaAdmin)
+admin.site.register(Producto,ProductoAdmin)
